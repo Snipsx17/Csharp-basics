d950e81c9ded3a55b0c6b230dbd2fd9661250505
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-06-27 21:01:10 -0400

    Basic data types

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿
+// console print
 Console.WriteLine("Hello, World!");
+
+// data types
+string name = "Uberth";
+char note = 'A';
+int age = 32;
+float distance = 1.2F;
+double PI = 3.14159;
+decimal money = 367.234567M;
+
